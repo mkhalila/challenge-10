fbca46bf2fce767131d10bc157296e9c06e0845d
Khalil Ahmed <mkhalila@outlook.com>
2016-12-14 01:02:36 +0000

    Adds printItOut implementation

--- partc/SomeClass.cpp
+++ partc/SomeClass.cpp
@@ -1,9 +1,14 @@
 #include "SomeClass.h"
+#include <iostream>
+using std::cout;
+using std::endl;
 
 int SomeClass::getX() const {
     return x;
 }
 
 // TODO: add code here
-
+void SomeClass::printItOut() const {
+	cout << getX() << endl;
+}
 
