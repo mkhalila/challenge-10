422cae4fb6305bb4f105a0f1040ecc89723ab1ce
Khalil Ahmed <mkhalila@outlook.com>
2016-12-14 00:46:56 +0000

    Implements initial attempt at romanToInt()

--- partb/Roman.cpp
+++ partb/Roman.cpp
@@ -1,1 +1,22 @@
 // TODO: implement romanToInteger in here
+int romanCharToInt(char & romanChar) {
+	if (romanChar == 'M') return 1000;
+	if (romanChar == 'D') return 500;
+	if (romanChar == 'C') return 100;
+	if (romanChar == 'L') return 50;
+	if (romanChar == 'X') return 10;
+	if (romanChar == 'V') return 5;
+	if (romanChar == 'I') return 1;
+}
+
+int romanToInteger(const string & s) {
+	int toReturn = 0; 
+
+	for (int i = 0; i < s.size()-1; ++i) {
+		if (romanCharToInt(s[i]) > romanCharToInt(s[i+1]))
+			toReturn += romanCharToInt(s[i]);
+		else toReturn -= romanCharToInt(s[i]);
+	}
+
+	return toReturn += romanCharToInt(s[s.size() - 1]);
+}
