7690373251989c17f74dd9cb28f0a73449c27afa
Khalil Ahmed <mkhalila@outlook.com>
2016-12-14 00:48:45 +0000

    Implements initial attempt at main

--- partb/main.cpp
+++ partb/main.cpp
@@ -1,4 +1,10 @@
 // TODO: write the main function in here
-
 #include "Roman.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << romanToInteger("MCMXIV") << endl;
+}
